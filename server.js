574b75ed4d4d9d0b47180d7b2234896ffb8497cd
Hunter <88465412+Hunyeh@users.noreply.github.com>
2021-10-26 12:16:07 -0400

    added more files and content

--- server.js
+++ server.js
@@ -7,9 +7,8 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
+app.use(express.static('public'));
 const { animals } = require('./data/animals.json');
-const { type } = require('os');
-
 
 
 filterByQuery = (query, animalsArray) => {
@@ -111,6 +110,23 @@ app.post('/api/animals', (req, res) => {
   }
 });
 
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/animals', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+app.get('/zookeepers', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './public.index.html'));
+});
+
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
 });
+
